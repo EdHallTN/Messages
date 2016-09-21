1ab970adaaea6b1ccc11289269d5468bc6e303a9
EdHall <rolltiger@gmail.com>
2016-09-21 08:27:59 -0500

    Initial commit

---
+++ src/Message.java
@@ -0,0 +1,15 @@
+/**
+ * Created by EdHall on 9/20/16.
+ */
+public class Message {
+    String message;
+
+    public Message(String message) {
+        this.message = message + "\n\n";
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,13 @@
+
+/**
+ * Created by EdHall on 9/20/16.
+ */
+public class User {
+    String name;
+
+
+    public User(String name) {
+        this.name = name;
+
+    }
+}
